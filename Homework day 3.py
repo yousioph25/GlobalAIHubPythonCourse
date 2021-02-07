d682fba71c56c2e0cb7b9c0d1cc6c6d9be78d1f1
yousioph25 <67334438+yousioph25@users.noreply.github.com>
2021-02-07 02:35:51 -0800

    Add files via upload

---
+++ Homework day 3.py
@@ -0,0 +1,10 @@
+User_name_database = ['Yousioph']
+password_database = ['0123456']
+
+if True:
+    entered_user_name = input("Please enter the user name:")
+    entered_password = input("please enter your password:")
+    if entered_user_name == 'Yousioph' and entered_password == '0123456':
+        print('login Successful,you will be shortly redirected to your account page')
+    else:
+        print('login fail,please try again\nif you cannot remember you should choose forget password below')
